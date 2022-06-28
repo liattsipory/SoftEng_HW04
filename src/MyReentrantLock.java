a6b30121683be72e26aab3d292a61d931529d9bb
Liat Tsipory <liat.tsipory@campus.technion.ac.il>
2022-06-28 20:35:04 +0300

    Continuing question 1 <3

--- src/MyReentrantLock.java
+++ src/MyReentrantLock.java
@@ -1,26 +1,51 @@
+import java.util.concurrent.atomic.AtomicBoolean;
 public class MyReentrantLock implements Lock {
-    private boolean isLocked = false;
-    private Thread lockedThread = null;
+    private AtomicBoolean isLocked = new AtomicBoolean(false);
+    private Thread lockedThread;
+    private int counter = 0;
+
+    public MyReentrantLock(){
+        this.lockedThread = Thread.currentThread();
+    }
+
 
     @Override
     public void acquire() {
-        super
-
+        if (this.counter==0 || Thread.currentThread()!=this.lockedThread) {
+            while (this.isLocked.get() && Thread.currentThread()!=this.lockedThread) {
+                try {
+                    Thread.sleep(11);
+                } catch (InterruptedException e) {
+                }
+            }
+            this.lockedThread = Thread.currentThread();
         }
-
-
+        this.counter++;
     }
 
     @Override
     public boolean tryAcquire() {
-        return false;
+        if (!isLocked.get() || (isLocked.get() && Thread.currentThread()==this.lockedThread)) {
+            acquire();
+            return true;
+        } else return false;
     }
 
     @Override
     public void release() {
+        if (!isLocked.get() || Thread.currentThread()!=this.lockedThread) {
+            throw new IllegalReleaseAttempt();
+        } else {
+            if (this.counter==1) {
+                this.counter--;
+                this.isLocked.set(false);
+            } if (this.counter > 1) this.counter--;
+        }
 
     }
 
     @Override
-    public void close()
+    public void close() {
+        this.release();
+    }
 }
